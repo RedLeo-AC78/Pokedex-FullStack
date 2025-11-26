32d8e6242ef7c2be23da215e880d8a88e5d01dd0
RedLeo-AC78 <168549123+RedLeo-AC78@users.noreply.github.com>
2025-11-26 17:41:23 +0100

    feat(backend): setup Express server with ESLint

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: ["dist", "node_modules"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      "no-unused-vars": "warn",
+    },
+  },
+]);
